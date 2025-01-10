3e4c032940c17becde8ce8b5ea5e26c0e118f38c
erbth <t.erbesdobler@gmx.de>
2025-01-11 00:15:16 +0100

    fix for vlan id 1

--- qemu-tap-ifup.py
+++ qemu-tap-ifup.py
@@ -22,7 +22,8 @@ def main():
     subprocess.run(['/sbin/bridge', 'vlan', 'add',
                     'vid', str(vlan), 'dev', dev, 'pvid', 'untagged']).check_returncode()
 
-    subprocess.run(['/sbin/bridge', 'vlan', 'del', 'vid', '1', 'dev', dev]).check_returncode()
+    if vlan != 1:
+        subprocess.run(['/sbin/bridge', 'vlan', 'del', 'vid', '1', 'dev', dev]).check_returncode()
 
     subprocess.run(['/sbin/ip', 'link', 'set', dev, 'up']).check_returncode()
 
